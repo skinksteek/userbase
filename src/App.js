a32d2ef5f293b5265fc25512422bb5eb38f5a743
Linus <linusigelstrom@gmail.com>
2024-09-17 19:10:06 +0200

    started working with supabase

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ function App() {
       <h1>Hello</h1>
       <form>
         <input type="text" placeholder="Namn" id="name" required></input>
-        <input type="text" placeholder="E-post" id="name" required></input>
-        <input type="number" placeholder="Ålder" id="name" required></input>
+        <input type="text" placeholder="E-post" id="email" required></input>
+        <input
+          type="number"
+          min="1"
+          max="100"
+          placeholder="Ålder"
+          id="age"
+          required
+        ></input>
       </form>
     </div>
   );

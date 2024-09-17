c56f30939e66fd22274652455254803ae41977bf
Linus <linusigelstrom@gmail.com>
2024-09-17 15:52:07 +0200

    edited readme and cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <form>
+        <input type="text" placeholder="Namn" id="name" required></input>
+        <input type="text" placeholder="E-post" id="name" required></input>
+        <input type="number" placeholder="Ålder" id="name" required></input>
+      </form>
     </div>
   );
 }

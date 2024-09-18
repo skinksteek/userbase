dbd5d5e4caded78520fd7cbc236cab96f9fbe7dd
Linus <linusigelstrom@gmail.com>
2024-09-18 18:28:04 +0200

    submit button

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <form>
-        <input type="text" placeholder="Namn" id="name" required></input>
-        <input type="text" placeholder="E-post" id="email" required></input>
+        <input type="text" placeholder="Name" id="name" required></input>
+        <input type="text" placeholder="Email" id="email" required></input>
         <input
           type="number"
           min="1"
           max="100"
-          placeholder="Ålder"
+          placeholder="Age"
           id="age"
           required
         ></input>
+        <button type="submit">Add user</button>
       </form>
     </div>
   );
